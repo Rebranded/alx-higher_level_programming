f3727b4384739413050a15b689149d3be232b222
Rebranded <akwaowombo@gmail.com>
2024-01-04 21:57:22 -0800

    updated

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-
-from sys import argv
-
+# Prints the number of and the list of its arguments
 if __name__ == "__main__":
-    # Get the number of arguments
-    num_arguments = len(argv) - 1  # Subtract 1 to exclude the script name
-
-    # Print the number of arguments and the list of arguments
-
-    print("Number of argument(s):", num_arguments, end=" ")
-    if num_arguments == 0:
-        print(".")
+    import sys
+    arg = sys.argv
+    size = len(arg) - 1
+    if size > 1:
+        print("{} arguments:".format(size))
+        for i in range(1, size + 1):
+            print("{}: {}".format(i, arg[i]))
+    elif size == 0:
+        print("{} arguments.".format(size))
     else:
-        print(":")
-
-        # Print details for each argument
-        for i in range(1, num_arguments + 1):
-            print(f"{i}: {argv[i]}")
+        print("{} argument:".format(size))
+        print("{}: {}".format(size, arg[1]))
